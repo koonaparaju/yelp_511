bd97d2b9d3b2de87291871751e150b1d259bbe77
James Lee <jameslee2@fb.com>
2019-12-10 22:54:59 -0800

    added new plots and updated data tables

--- App.R
+++ App.R
@@ -11,7 +11,8 @@ names(r_colors) <- colors()
 
 business_csv <- 'data/business_parsed.csv'
 result <- fread(business_csv, header = TRUE)
-categories_list = unique(unlist(strsplit(result$category, split = ":"), recursive = FALSE))
+categories_list = sort(unique(unlist(strsplit(result$category, split = ":"), recursive = FALSE)))
+result[,category:=gsub(':', ', ', category)]
 result[Grade == 1, Sanitation := 'Excellent']
 result[Grade == 2, Sanitation := 'Good']
 result[Grade == 3, Sanitation := 'Okay']
@@ -41,7 +42,7 @@ body <- dashboardBody(
     ),
     column(width = 3,
            selectInput('location', label = 'Location', choices = unique(result$Neighborhood), multiple=TRUE, selected = 'Pioneer Square'),
-           selectInput('category', label = 'Cuisine', choices = categories_list, multiple = TRUE, selected='Asian Fusion'),
+           selectInput('category', label = 'Cuisine', choices = categories_list, multiple = TRUE),
            selectInput('sanitation', label = 'Sanitation', choices = unique(result$`Food Safety Rating`), multiple=TRUE, selectize=TRUE, selected = 'Good'),
            selectInput('price', label = 'Price', choices = unique(result$Price),multiple=TRUE, selectize=TRUE, selected = c('','$','$$','$$$','$$$$')),
            sliderInput("review", label = "Rating", min = 0, max = 5, value = c(4, 5)),
@@ -54,7 +55,12 @@ body <- dashboardBody(
       # The id lets us use input$tabset1 on the server to find the current tab
       id = "tabset1", height = "250px",
       tabPanel("Data Table", DT::dataTableOutput('table')),
-      tabPanel("Drill Down", drilldown )
+      tabPanel("Drill Down", fluidRow(
+        column(width = 9,
+               plotlyOutput("barchart")),
+        column(width = 3,
+               selectInput('topic', label = 'Drill Down Topic', choices = c('Rating','Price','Food Safety Rating', 'Neighborhood','Cuisine'), selected = 'Rating'))
+      ) )
     )
   )
 )
@@ -99,25 +105,37 @@ server <- function(input, output, session){
       addAwesomeMarkers(~longitude, ~latitude, icon=icons, popup = ~(popuphtml))
   })
   
-  output$barchart <-renderC3BarChart({
-    newdata = reactiveresult()
-    cat(file=stderr(), "drawing histogram with",length(newdata$Rating), "bins", "\n")
-    dataset <- count(newdata, Rating, name='Rating Count')
-    cat(file=stderr(), "drawing histogram with",length(dataset), "bins", "\n")
-    C3BarChart(dataset, 'Rating')
-  })
-  
-  output$sanitation <-renderC3BarChart({
-    newdata = reactiveresult()
-    cat(file=stderr(), "drawing histogram with",length(newdata$Rating), "bins", "\n")
-    dataset <- count(newdata, Price, name='count')
-    setnames(dataset, c('Price','count'))
-    cat(file=stderr(), "drawing histogram with",length(dataset), "bins", "\n")
-    C3BarChart(dataset, 'Price')
+  # output$barchart <-renderC3BarChart({
+  #   newdata = reactiveresult()
+  #   cat(file=stderr(), "drawing histogram with",length(newdata$Rating), "bins", "\n")
+  #   dataset <- count(newdata, Rating, name='Rating Count')
+  #   cat(file=stderr(), "drawing histogram with",length(dataset), "bins", "\n")
+  #   C3BarChart(dataset, 'Rating')
+  # })
+  # 
+  # output$sanitation <-renderC3BarChart({
+  #   newdata = reactiveresult()
+  #   cat(file=stderr(), "drawing histogram with",length(newdata$Rating), "bins", "\n")
+  #   dataset <- count(newdata, Price, name='count')
+  #   setnames(dataset, c('Price','count'))
+  #   cat(file=stderr(), "drawing histogram with",length(dataset), "bins", "\n")
+  #   C3BarChart(dataset, 'Price')
+  # })
+  output$barchart = renderPlotly({
+    topic = input$topic
+    dataset <- count(reactiveresult(), get(topic), name='metric')
+    setnames(dataset,'get(topic)', topic)
+    dataset[[topic]] = as.factor(dataset[[topic]])
+    print(plot_ly(dataset, x = ~get(topic), y = ~metric, 
+            type = 'bar', name = ~paste0("Distribution of ",topic),
+            hoverinfo = 'text',
+            text = ~paste0(topic,' ',get(topic),
+                          ': </br></br>', metric)))
   })
 
+
   
-  output$table = DT::renderDataTable(reactiveresult(), server = TRUE)
+  output$table = DT::renderDataTable(reactiveresult()[,!c('latitude','longitude','popuphtml')], server = TRUE)
   
   observeEvent(input$pie1,{
     
